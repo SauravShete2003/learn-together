2a8add4a56e34e59e768ee8b681d2fee23d5b5fc
sauravshete2003 <sauravshete72@gmail.com>
2024-11-17 09:13:28 +0530

    add singup and login logic and design

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
+import toast from "react-hot-toast";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await api.post("/login", { email, password });
+      if (response) {
+        toast.success(response.data.message);
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("user", JSON.stringify(response.data.user));
+        setTimeout(() => {
+          window.location.href = "/";
+        }, 2000);
+      }
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>
-      Login Page
+    <div className="auth-container">
+      <h1 className="auth-heading">
+        👋 Welcome Back! Sign in to unlock a world of personalized experiences
+        crafted just for you. ✨
+      </h1>
+      <div className="auth-form-contianer">
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="auth-input"
+        />
+        <input 
+        type="password" 
+        placeholder="Password" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="auth-input" />
+
+        <button type="button" className="auth-btn" onClick={handleLogin}>
+          Sign In
+        </button>
+        <div>
+          Don't have an account? <Link to="/signup">Sign up</Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- client/src/views/Signup/Signup.jsx
+++ client/src/views/Signup/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import api from "./../../services/api";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 function Signup() {
   const [formData, setFromData] = useState({
@@ -11,8 +12,13 @@ function Signup() {
 
   const handleSubmit = async () => {
     try {
-      const response = await api.post("/signup", formData);
+      const response = await api.post("/register", formData);
+      console.log("Response:", response.data);
+      console.log("Form Data:", formData);
       toast.success("Signup Successfull");
+      setTimeout(() => {
+        window.location.href = "/login";
+      }, 2000);
       console.log(response.data);
     } catch (error) {
       toast.error("Signup Failed");
@@ -20,11 +26,14 @@ function Signup() {
   };
 
   return (
-    <div>
-      Signup Page
-      <div>
+    <div className="auth-container">
+      <h1 className="auth-heading">
+        👋 Welcome to Learn Together Level Up Your Skills Together! 🚀
+      </h1>
+      <div className="auth-form-container">
         <input
           type="text"
+          className="auth-input"
           placeholder="Enter FullName"
           value={formData.name}
           onChange={(e) => setFromData({ ...formData, name: e.target.value })}
@@ -32,21 +41,25 @@ function Signup() {
 
         <input
           type="email"
+          className="auth-input"
           value={formData.email}
           placeholder="Enter Your Email"
           onChange={(e) => setFromData({ ...formData, email: e.target.value })}
         />
         <input
           type="password"
+          className="auth-input"
           value={formData.password}
           placeholder="Enter Your Password"
           onChange={(e) =>
-            setFromData({ ...formData, password: e.target.value })
-          }
-        />
-        <button type="button" onClick={handleSubmit}>
+            setFromData({ ...formData, password: e.target.value })} />
+
+        <button type="button" className="auth-btn" onClick={handleSubmit}>
           Register
         </button>
+        <div>
+          Already have an account? <Link to={'/login'}>Login</Link> 
+        </div>
       </div>
     </div>
   );
